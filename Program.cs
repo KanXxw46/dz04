2e6e146b36566a09d5b5916165bf698004c5bf07
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-09-20 10:58:28 +0600

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+
+namespace dz_4
+{
+    class Program
+    {
+        static string connectionString = @"Server = (localdb)\\mssqllocaldb;Database = OnlineShop;Trusted_Connection=True;";
+        static SqlConnection sqlConnection = null;
+        static SqlCommand sqlCommand = null;
+        static SqlDataReader reader = null;
+        static void Main(string[] args)
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            sqlCommand = new SqlCommand
+            ("create table gruppa (Id int primary key identity, Name nvarchar(50) not null)", sqlConnection);
+            sqlCommand.ExecuteNonQuery();
+            Console.ReadKey();
+        }
+    }
+}
